060258db2714e7a1506a81a4d63fe3fe358f5faf
Levy Kohout <LevyKohout@Levy-Kohout.local>
2016-10-03 15:50:53 -0500

    change css jquery background color to addClass

--- app.js
+++ app.js
@@ -22,25 +22,25 @@ console.log('try to create a box');
       console.log($color);
 
   if ($color == 'red'){
-    $box.css('background-color','red');
+     $box.addClass('red'); //css('background-color','red');
     redCounter++;
     $('#red').find('span').remove();
     $('#red').append('<span>'+ redCounter +'</span>');
 
   } else if ($color == 'yellow'){
-      $box.css('background-color','yellow');
+      $box.addClass('yellow');    //css('background-color','yellow');
       yellowCounter++;
       $('#yellow').find('span').remove();
       $('#yellow').append('<span>'+ yellowCounter +'</span>');
 
   } else if ($color == 'green'){
-        $box.css('background-color','green');
+        $box.addClass('green');   //css('background-color','green');
         greenCounter++;
         $('#green').find('span').remove();
         $('#green').append('<span>'+ greenCounter +'</span>');
 
   } else if ($color == 'blue'){
-          $box.css('background-color','blue');
+          $box.addClass('blue');   //css('background-color','blue');
           blueCounter++;
           $('#blue').find('span').remove();
           $('#blue').append('<span>'+ blueCounter +'</span>');
